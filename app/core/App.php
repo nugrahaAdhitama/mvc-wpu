7704ce68c4822d6ed906bb945e1855fe307932e3
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2022-01-04 19:47:13 +0700

    Add files via upload
    menambahkan file pelajaran hari ini

---
+++ app/core/App.php
@@ -0,0 +1,42 @@
+<?php
+
+class App {
+    // properti untuk menentukan controller, method, dan parameter default
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
+    public function __construct () {
+        $url = $this->parseURL();
+
+        // cek ada ga sebuah file di dalam folder controllers yang namanya sesuai dengan nama yang ditulis di URL
+        if(file_exists('../app/controllers/'. $url[0] . '.php')){
+            $this->controller = $url[0];
+            // kita hilangkan controllernya dari elemen array nya ini supaya kita bisa apus parameternya
+            unset($url[0]);
+        }
+
+        // panggil controllernya
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        // bikin logic yang sama buat method
+        if( isset($url[1]) ) {
+            // cek method dari controller yang baru di instance
+            if( method_exists($this->controller, $url[1]) ) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+    }
+
+    // membuat method yang bertugas untuk mengambil URL dan memecah sesuai keinginan kita
+    public function parseURL() {
+        if( isset($_GET['url']) ){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
+}
