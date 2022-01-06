9eb4bd1726a758c432dfffdfe2d89364220ab6cd
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2022-01-06 19:11:00 +0700

    menyelesaikan pengelolaan aset yang ada pada website sederhana ini

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About me</h1>
-<p>Halo, nama saya <?= $data['nama']; ?>, saya seorang <?= $data['pekerjaan'] ?></p>
+<div class="container">
+    <h1 class="mt-4">About me</h1>
+    <img src="<?= BASEURL; ?>/img/9a1e359.jpg" alt="Profile Picture" width="200" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, saya seorang <?= $data['pekerjaan'] ?></p>
+</div>
