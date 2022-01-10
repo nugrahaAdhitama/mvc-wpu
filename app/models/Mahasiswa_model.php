e3d45c5e1b3e90238f2e45af4e08db2d55e90708
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2022-01-10 17:33:27 +0700

    menambahkan fitur data-wrapper untuk aplikasi MVC

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,25 +1,23 @@
 <?php
 
 class Mahasiswa_model{
-    // membuat koneksi PDO
-    private $dbh;
-    private $stmt;
+    private $table = 'mahasiswa';
+    // variabel untuk menampung kelas db
+    private $db;
 
-    // lakukan koneksi ke database dalam method construct
     public function __construct() {
-        // data source name
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-        try {
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch(PDOException $e) {
-            die($e->getMessage());
-        }
+        $this->db = new Database;
     }
 
     public function getAllMahasiswa() {
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->db->query('SELECT * FROM ' . $this->table);
+
+        return $this->db->resultSet();
+    }
+
+    public function getMahasiswaById($id) {
+        $this->db->query('SELECT * FROM '. $this->table. ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
     }
 }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -2,22 +2,14 @@
     <div class="row">
         <div class="col-6">
             <h3>Daftar Mahasiswa</h3>
-                <?php foreach($data['mhs'] as $mhs) : ?>
-                    <ul>
-                        <li>
+                <ul class="list-group">
+                    <?php foreach($data['mhs'] as $mhs) : ?>
+                        <li class="list-group-item d-flex justify-content-between align-items-start">
                             <?= $mhs['nama']; ?>
+                            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge bg-primary">Detail</a>
                         </li>
-                        <li>
-                            <?= $mhs['email']; ?>
-                        </li>
-                        <li>
-                            <?= $mhs['jurusan']; ?>
-                        </li>
-                        <li>
-                            <?= $mhs['NIS']; ?>
-                        </li>
-                    </ul>
-                <?php endforeach; ?>
+                    <?php endforeach; ?>
+                </ul>
         </div>
     </div>
 </div>
